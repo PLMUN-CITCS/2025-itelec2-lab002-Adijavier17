1a64e53a3475598b19a490db57b36bd0a53a6849
Adi <javieradrian_bsit@plmun.edu.ph>
2025-03-10 21:56:23 +0800

    Submitting Python Session 03 - Exercise 01

--- problem_01.py
+++ problem_01.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
 print("Simple Calculator Program")
 
 num1 = int(input("Enter 1st number: "))
+
 num2 = int(input("Enter 2nd number: "))
 
 print(f"The sum is {num1 + num2}")
